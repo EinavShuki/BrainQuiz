c30f120b44354ed47908ab453040fe392dfc700e
Stav Elmashally <stavelmashally6@gmail.com>
2020-12-14 19:56:14 +0200

    Added Firebase Database

--- app/src/main/java/com/example/brainquiz/MainActivity.java
+++ app/src/main/java/com/example/brainquiz/MainActivity.java
@@ -1,8 +1,16 @@
 package com.example.brainquiz;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +19,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        readFromDb();
+    }
+
+    private void readFromDb() {
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("scoreboard");
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()){
+                    Log.i("DB result:" + snapshot.getKey(), snapshot.getValue().toString());
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
