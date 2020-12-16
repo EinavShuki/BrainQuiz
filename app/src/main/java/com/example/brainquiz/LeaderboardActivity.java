f0045966a530f2a7e725f2123a22cbaaa80f7ba8
Stav Elmashally <stavelmashally6@gmail.com>
2020-12-16 19:15:49 +0200

    Tab Layout

--- app/src/main/java/com/example/brainquiz/LeaderboardActivity.java
+++ app/src/main/java/com/example/brainquiz/LeaderboardActivity.java
@@ -1,14 +1,33 @@
 package com.example.brainquiz;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabLayout;
+
 public class LeaderboardActivity extends AppCompatActivity {
+
+    private TabAdapter adapter;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_leaderboard);
+
+        viewPager = findViewById(R.id.viewPager);
+        tabLayout = findViewById(R.id.tabLayout);
+
+        adapter = new TabAdapter(getSupportFragmentManager());
+        adapter.addFragment(new MathScoresFragment(), "Math");
+        adapter.addFragment(new NumbersMemoryScoresFragment(), "Numbers Memory");
+        adapter.addFragment(new VisualMemoryScoreFragment(), "Visual Memory");
+        adapter.addFragment(new SequenceScoreFragment(), "Sequence");
+
+        viewPager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(viewPager);
     }
 }
