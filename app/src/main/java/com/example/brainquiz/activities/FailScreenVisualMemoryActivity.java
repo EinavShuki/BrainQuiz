f1d11247c86e13f4d15eb6f3939d04d393c287c1
Einav1 <einavshpigel@gmail.com>
2020-12-21 23:29:46 +0200

    create VisualMemory game

--- app/src/main/java/com/example/brainquiz/activities/FailScreenVisualMemoryActivity.java
+++ app/src/main/java/com/example/brainquiz/activities/FailScreenVisualMemoryActivity.java
@@ -1,8 +1,10 @@
-package com.example.brainquiz;
+package com.example.brainquiz.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.brainquiz.R;
 
 public class FailScreenVisualMemoryActivity extends AppCompatActivity {
 
--- app/src/main/java/com/example/brainquiz/activities/VisualMemoryActivity.java
+++ app/src/main/java/com/example/brainquiz/activities/VisualMemoryActivity.java
@@ -1,4 +1,4 @@
-package com.example.brainquiz;
+package com.example.brainquiz.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,6 +10,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.GridLayout;
+
+import com.example.brainquiz.R;
 
 import java.util.ArrayList;
 import java.util.HashMap;
