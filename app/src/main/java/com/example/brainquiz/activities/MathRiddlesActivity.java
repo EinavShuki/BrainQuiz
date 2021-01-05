5d439dd899331a6c14f905f7c94ddb2307e849a4
sara_levi <59117049+saralevi96@users.noreply.github.com>
2021-01-05 19:19:19 +0200

    Added riddles and levels

--- app/src/main/java/com/example/brainquiz/activities/MathRiddlesActivity.java
+++ app/src/main/java/com/example/brainquiz/activities/MathRiddlesActivity.java
@@ -1,11 +1,14 @@
 package com.example.brainquiz.activities;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.util.Log;
 import android.util.Pair;
 import android.view.View;
 import android.widget.Button;
@@ -16,6 +19,7 @@ import android.widget.TextView;
 import com.example.brainquiz.R;
 import com.example.brainquiz.utils.Constants;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
@@ -25,9 +29,10 @@ import java.util.Random;
 public class
 MathRiddlesActivity extends AppCompatActivity implements View.OnClickListener {
     SharedPreferences sp;
-    List<Pair<String, String>> levelStart = Constants.riddlesLevelStart;
-    List<Pair<String, String>> levelMiddle = Constants.riddlesLevelMiddle;
-    List<Pair<String, String>> levelHigh = Constants.riddlesLevelHigh;
+    List<Pair<String, String>> levelStart = new ArrayList<>(Constants.riddlesLevelStart);
+    List<Pair<String, String>> levelMiddle = new ArrayList<>(Constants.riddlesLevelMiddle);
+    List<Pair<String, String>> levelHigh = new ArrayList<>(Constants.riddlesLevelHigh);
+
     String[] ecersice = {"", ""};
 
     long time;
@@ -66,10 +71,13 @@ MathRiddlesActivity extends AppCompatActivity implements View.OnClickListener {
 
 
 
-        new CountDownTimer(11000, 1000) {
+        new CountDownTimer(61000, 1000) {
 
             public void onTick(long millisUntilFinished) {
                 Timer.setText(String.format("%02d:%02d", millisUntilFinished/1000/ 60, millisUntilFinished /1000% 60));
+                if(millisUntilFinished/1000% 60==10){
+                    Timer.setTextColor(getColor(R.color.red));
+                }
                 time = millisUntilFinished /1000% 60;
             }
 
@@ -122,21 +130,25 @@ MathRiddlesActivity extends AppCompatActivity implements View.OnClickListener {
             if(!tvAnswer.getText().toString().equals("")){
                 if(tvAnswer.getText().toString().equals(ecersice[1])){
                     Count.setText( String.valueOf(Integer.parseInt(Count.getText().toString())+1));
-                    if( time>40) {
+                    if( time>40 && Integer.parseInt(Count.getText().toString())<5){
                         random_num = new Random().nextInt(levelStart.size());
                         ecersice[0] = levelStart.get(random_num).first;
                         ecersice[1] = levelStart.get(random_num).second;
+                        levelStart.remove(random_num);
+                        Log.i("sssiiizzeee", String.valueOf(levelStart.size()));
 
                     }
-                    else if(time<=40 && time>20) {
+                    else if((time<=40 && time>20 || (Integer.parseInt(Count.getText().toString())>=5)) && Integer.parseInt(Count.getText().toString())<10) {
                         random_num = new Random().nextInt(levelMiddle.size());
                         ecersice[0] = levelMiddle.get(random_num).first;
                         ecersice[1] = levelMiddle.get(random_num).second;
+                        levelMiddle.remove(random_num);
                     }
                     else{
                         random_num = new Random().nextInt(levelHigh.size());
                         ecersice[0] = levelHigh.get(random_num).first;
                         ecersice[1] = levelHigh.get(random_num).second;
+                        levelHigh.remove(random_num);
                     }
                     Calculate.setText(ecersice[0]);
                     tvAnswer.setText("");
@@ -153,6 +165,7 @@ MathRiddlesActivity extends AppCompatActivity implements View.OnClickListener {
 //    protected void onPause() {
 //        super.onPause();
 //        SharedPreferences.Editor editor = sp.edit();
+//
 //        editor.apply();
 //    }
 
@@ -168,6 +181,12 @@ MathRiddlesActivity extends AppCompatActivity implements View.OnClickListener {
 
     }
 
+    @Override
+    public void onBackPressed() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        builder.setTitle("Confirm shutdown").setMessage("are you yiyuihj").setPositiveButton("yes,get out", (dialog, which) -> MathRiddlesActivity.super.onBackPressed()).setNegativeButton("stay..",null).show();
+    }
 
     @Override
     public void onClick(View v) {
@@ -198,6 +217,9 @@ MathRiddlesActivity extends AppCompatActivity implements View.OnClickListener {
                 break;
             case R.id.btn_9:
                 appendNumber("9");
+                break;
+            case R.id.btn_0:
+                appendNumber("0");
                 break;
             case R.id.tv_x:
                 tvAnswer.setText("");
--- app/src/main/java/com/example/brainquiz/utils/Constants.java
+++ app/src/main/java/com/example/brainquiz/utils/Constants.java
@@ -22,28 +22,44 @@ public class Constants {
     public static final String TABLE_KEY = "TABLE";
     public static final String USERNAME_PREFS = "UserName";
     public static final String DIALOG_SAVE_SCORE = "save score dialog";
+    public static final char pow_2 = '\u00B2';
+    public static final char pow_5 = '\u2075';
+    public static final char pow_0 = '\u2070';
+    public static final char pow_1 = '\u00B9';
+    public static final char pow_9 = '\u2079';
+    public static final char pow_3 = '\u00B3';
 
     public static List<Pair<String, String>> riddlesLevelStart = Arrays.asList(
-            new Pair<>("5+2=", "7"),
-            new Pair<>("5+3=", "8"),
-            new Pair<>("10+5=", "15"),
-            new Pair<>("9/3", "3"),
-            new Pair<>("3", "4"),
-            new Pair<>("4", "4"),
-            new Pair<>("5", "4"),
-            new Pair<>("6", "4"),
-            new Pair<>("7", "4"),
-            new Pair<>("38", "4")
+            new Pair<>("13 - 6 = ", "7"),
+            new Pair<>("48 - 9 =", "39"),
+            new Pair<>("10 + 5 =" ,"15"),
+            new Pair<>("9 / 3 =", "3"),
+            new Pair<>("8 X 7 =", "56"),
+            new Pair<>("12 X 10 =", "120"),
+            new Pair<>("5 X 5 - 5 =", "20"),
+            new Pair<>("7 X 4 = ", "28"),
+            new Pair<>("7 X 3 =", "21"),
+            new Pair<>("9 X 5 =", "45"),
+            new Pair<>("2 + 2 X 3 =", "8"),
+            new Pair<>("56 - 6 =", "50"),
+            new Pair<>("7 - 3 = ", "4"),
+            new Pair<>("69 - 4 =", "65"),
+            new Pair<>("35 + 11 =", "46")
     );
     public static List<Pair<String, String>> riddlesLevelMiddle = Arrays.asList(
-            new Pair<>("20", "4"),
-            new Pair<>("20", "4"),
-            new Pair<>("20", "4")
+            new Pair<>("8 - 8 / 4 X 3 =", "2"),
+            new Pair<>("√(9)", "3"),
+            new Pair<>("21,18,15,?", "12"),
+            new Pair<>("√(64)", "8"),
+            new Pair<>("4, 11, 18, ?", "25")
 
     );
     public static List<Pair<String, String>> riddlesLevelHigh = Arrays.asList(
-            new Pair<>("30", "4"),
-            new Pair<>("30", "4"),
-            new Pair<>("30", "4")
+            new Pair<>("1" + pow_5 + " + " + "5" + pow_2 + " / "+"25" + pow_0 + " - "+"5" + pow_1, "21"),
+            new Pair<>("10 - √(9) X 2 = ", "4"),
+            new Pair<>("(3" + pow_2 + " - " + "2" + pow_3 +  ")" + pow_9 + " =", "1"),
+            new Pair<>("2,3,5,8,13,?", "21")
+
     );
+
 }
